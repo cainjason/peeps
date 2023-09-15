407b9e3450675d28d2a2af3d6baea5aeb47044e4
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-09-15 01:13:06 -0400

    Chore: created components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-function Greeting(){
-    return <h4> This is Jason and this is my first component </h4>;
+function Greeting () {
+    return (
+        <div>
+            <h4> This is Jason and this is my first component! </h4>
+            <input type="text" placeholder="First Name" />
+            <h2>Wild One</h2>
+        </div>
+    )
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root')
